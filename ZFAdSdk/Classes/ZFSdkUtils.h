40b8b02c13479f47a6dd14571192ac9a96190176
jusha <jusha@jusha2020deiMac.local>
2021-01-07 17:49:27 +0800

    add ZFSdkUtils

---
+++ ZFAdSdk/Classes/ZFSdkUtils.h
@@ -0,0 +1,18 @@
+//
+//  ZFSdkUtils.h
+//  Pods
+//
+//  Created by jusha on 2021/1/7.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZFSdkUtils : NSObject
+
+-(void)initSDK;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZFAdSdk/Classes/ZFSdkUtils.m
@@ -0,0 +1,16 @@
+//
+//  ZFSdkUtils.m
+//  Pods
+//
+//  Created by jusha on 2021/1/7.
+//
+
+#import "ZFSdkUtils.h"
+
+@implementation ZFSdkUtils
+
+- (void)initSDK {
+    NSLog(@"sdk inited successfully");
+}
+
+@end
