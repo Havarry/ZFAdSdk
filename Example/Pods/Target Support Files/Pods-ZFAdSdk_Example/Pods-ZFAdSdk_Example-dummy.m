b224dbce1f683416fd4458d66649bfbbaa469557
jusha <jusha@jusha2020deiMac.local>
2021-01-08 10:58:21 +0800

    delete remote pods dir

--- Example/Pods/Target Support Files/Pods-ZFAdSdk_Example/Pods-ZFAdSdk_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZFAdSdk_Example : NSObject
-@end
-@implementation PodsDummy_Pods_ZFAdSdk_Example
-@end
--- Example/Pods/Target Support Files/ZFAdSdk/ZFAdSdk-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ZFAdSdk : NSObject
-@end
-@implementation PodsDummy_ZFAdSdk
-@end
